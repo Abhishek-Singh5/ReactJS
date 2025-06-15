85fa04e00f38bc8f4b04c4f7aa62102e874d3edb
Abhishek Singh <96238644+Abhishek-Singh5@users.noreply.github.com>
2025-06-15 11:01:08 +0530

    Create App.jsx

---
+++ Day_2/Vite/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+      <Chai/>
+
+      <p>Hello this is Paragraph</p>
+
+      <h1>Main Heading</h1>
+     </>
+  )
+}
+
+export default App
